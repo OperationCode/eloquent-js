392271637cd3d359c694a6a530a4e9d46898600b
CodeNeophyte <gonz00ny@gmail.com>
2016-06-04 00:10:37 -1000

    Chapter 2 solutions

---
+++ CH2_EX1_Triangle.js
@@ -0,0 +1,10 @@
+function triangleLoop () {
+	var hash = '#';
+	for (var i=0; i < 7; i++) {
+		
+		console.log(hash);
+		hash += '#';
+
+	}
+
+}
---
+++ CH2_EX3_ChessBoard.js
@@ -0,0 +1,7 @@
+function chessBoard (size) {
+	var size = size / 2;
+		for(var i=0; i<size; i++) {
+			console.log('# # # #');
+			console.log(' # # # #');
+		}//for end
+}//function end
