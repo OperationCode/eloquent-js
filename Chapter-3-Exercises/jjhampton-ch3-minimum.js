8b05211340d3b0e7686abc69bbceacc8ab5117ee
John Hampton <john_j_hampton@yahoo.com>
2016-06-11 22:23:24 -0400

    Add solution for 'Minimum' exercise

---
+++ Chapter-3-Exercises/jjhampton-ch3-minimum.js
@@ -0,0 +1,19 @@
+'use strict'
+//Write a function min that takes two arguments and returns their minimum.
+
+function min(num1, num2) {
+    if (typeof num1 === 'number' && typeof num2 === 'number')
+        return num1 <= num2 ? num1 : num2;
+    else
+        return "Please enter numbers for both arguments."
+}
+
+
+// testing with a variety of arguments
+console.log(min(0, 10));  
+console.log(min(0, -10));
+console.log(min(-11, -10));
+console.log(min(11, 10));
+console.log(min(10, 10));
+console.log(min('a', -10)); // should not calculate minimum, return string instead
+
