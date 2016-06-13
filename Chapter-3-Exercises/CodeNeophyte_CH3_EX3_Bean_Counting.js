8d17ce51eadfee119205d95b5b847cd642c688e1
CodeNeophyte <gonz00ny@gmail.com>
2016-06-12 20:38:13 -1000

    Changed B to character
    A little more meaningful than just B.

--- Chapter-3-Exercises/CodeNeophyte_CH3_EX3_Bean_Counting.js
+++ Chapter-3-Exercises/CodeNeophyte_CH3_EX3_Bean_Counting.js
@@ -8,10 +8,10 @@ function countBs (string) {
 	return numberBs;
 }
 
-function countChar (string, B) {
+function countChar (string, character) {
 	var numberBs = 0;
 	for (var i=0; i < string.length; i++) {
-		if (string.charAt(i) === B) {
+		if (string.charAt(i) === character) {
 			numberBs += 1;
 		}
 	}
