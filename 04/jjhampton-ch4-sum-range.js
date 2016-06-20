3fe85a9ad40c61fe37b856cc0110f11d4eff861e
John Hampton <john_j_hampton@yahoo.com>
2016-06-19 21:16:45 -0400

    Add sum function
    Call sum with result of range(1, 10)

--- 04/jjhampton-ch4-sum-range.js
+++ 04/jjhampton-ch4-sum-range.js
@@ -9,7 +9,6 @@
 // As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used to build up the array. If no step is given, the array elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
 
 function range(start, end) {
-    // do stuff here
     const startToEnd = [];
     for (let i = start; i <= end; i++) {
         startToEnd.push(i);
@@ -18,4 +17,16 @@ function range(start, end) {
 }
 
 console.log(range(1,10));
+
+function sum(arrayOfNumbers) {
+    let sumOfNumbers = arrayOfNumbers[0];
+    const arrayLength = arrayOfNumbers.length;
+    for (let i = 1; i < arrayLength; i++) {
+        sumOfNumbers = sumOfNumbers + arrayOfNumbers[i];
+    }
+    return sumOfNumbers;
+}
+
+console.log(sum(range(1,10)));
+
 
