5b8e6a1439894563fe92b5517583bf48fff5f17a
John Hampton <john_j_hampton@yahoo.com>
2016-06-25 17:47:19 -0400

    Perform last-minute syntax refactoring/cleanup

--- 04/jjhampton-ch4-linked-list.js
+++ 04/jjhampton-ch4-linked-list.js
@@ -1,7 +1,6 @@
 // Write a function arrayToList that builds up a data structure like the previous one when given [1, 2, 3] as argument, and write a listToArray function that produces an array from a list. Also write the helper functions prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list, or undefined when there is no such element.
 
 function arrayToList(array) {
-    const arrayLength = array.length;
     let list = null;
     for (let i = array.length - 1; i >= 0; i-- ) {
         list = prepend(array[i], list);
@@ -19,22 +18,17 @@ function listToArray(list) {
 }
 
 function prepend(element, list) {
-    let newList = {
-            value: element,
-            rest: list
-        };
-    return newList;
+    return {
+        value: element,
+        rest: list
+    };
 }
 
 function nth(list, number) {
     if (!number) 
         return list.value;
 
-    number--;
-    if (list.rest)
-        return nth(list.rest, number)
-
-    return undefined;
+    return list.rest ? nth(list.rest, number - 1) : undefined;
 }
 
 const theArray = [1, 2, 3 ];
