2f18439322525fca5649d584f398ff636332f8f6
John Hampton <john_j_hampton@yahoo.com>
2016-06-25 14:15:01 -0400

    Add for-loop to arrayToList to allow for array arguments of dynamic length

--- 04/jjhampton-ch4-list.js
+++ 04/jjhampton-ch4-list.js
@@ -1,20 +1,18 @@
 // Write a function arrayToList that builds up a data structure like the previous one when given [1, 2, 3] as argument, and write a listToArray function that produces an array from a list. Also write the helper functions prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list, or undefined when there is no such element.
 
 function arrayToList(array) {
-    const list = {
-        value: array[0],
-        rest: {
-            value: array[1],
-            rest: {
-                value: array[2],
-                rest: null
-            }
-        }
-    };
+    const arrayLength = array.length;
+    let list = null;
+    for (let i = array.length - 1; i >= 0; i-- ) {
+        list = {
+            value: array[i],
+            rest: list
+        };
+    }
     return list;
 }
 
-const theArray = [1, 2, 3];
+const theArray = [1, 2, 3, 4, 5, 6, 7, 8];
 console.log(arrayToList(theArray));
 
 function listToArray(list) {
@@ -27,5 +25,14 @@ function listToArray(list) {
     return array;
 }
 
-const theList = arrayToList([1, 2, 3]);
-console.log(listToArray(theList));
+// const theList = arrayToList([1, 2, 3]);
+// console.log(listToArray(theList));
+
+// write helper function 'prepend', which takes an element and a list and creates a new list that adds the element to the front of the input list
+
+function prepend(element, list) {
+    let newList;
+
+    // do some stuff here
+    return newList;
+}
